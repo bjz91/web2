127a7e66c80e80978525f04e61a58513a4aa73a2
Bijianzhao <bjz91@qq.com>
2015-10-12 16:41:50 +0800

    v2.2.4 修改bug

---
+++ pages/regiondis/init.js
@@ -0,0 +1,165 @@
+function initComponent() {
+
+	var fileNameBar = 'data/SO2-bar.json';
+
+	/*--------- 加载ECharts ---------*/
+	$.getJSON(fileNameBar, function(bardata) {
+
+		//动态生成下拉采菜单
+		var option1 = document.getElementById('sel1');
+		var option2 = document.getElementById('sel2');
+		for (var i = 0; i < bardata.bar.data.name.length; i++) {
+			if (i === 0) {
+				option1.options[i].value = i;
+				option1.options[i].text = bardata.bar.data.name[i];
+				option2.options[i].value = i;
+				option2.options[i].text = bardata.bar.data.name[i];
+			} else {
+				var objOption1 = document.createElement("OPTION");
+				objOption1.value = i;
+				objOption1.text = bardata.bar.data.name[i];
+				option1.options.add(objOption1);
+				var objOption2 = document.createElement("OPTION");
+				objOption2.value = i;
+				objOption2.text = bardata.bar.data.name[i];
+				option2.options.add(objOption2);
+			}
+		}
+		option1.options[0].selected = true;
+		//默认选中第0个，与html文件里的默认选项对应
+		option2.options[1].selected = true;
+		//默认选中第1个，与html文件里的默认选项对应
+
+		loadComponent(bardata);
+	});
+
+}
+
+function initPie1() {
+
+	var fileNameBar = 'data/SO2-bar.json';
+	var fileNamePie = 'data/SO2-pie.json';
+	var cityIdx = document.getElementById('sel1').value;
+	var divName = 'container1';
+
+	/*--------- 加载ECharts ---------*/
+	$.getJSON(fileNameBar, function(bardata) {
+		$.getJSON(fileNamePie, function(piedata) {
+			//合并到其他
+			var comObj = combine(bardata, piedata, 80, cityIdx);
+			var barDataObj = comObj.barDataObj;
+			var pieDataObj = comObj.pieDataObj;
+			var map = comObj.map;
+			loadPie(bardata, piedata, cityIdx, divName, barDataObj, pieDataObj, map);
+		});
+	});
+
+}
+
+function initPie2() {
+
+	var fileNameBar = 'data/SO2-bar.json';
+	var fileNamePie = 'data/SO2-pie.json';
+	var cityIdx = document.getElementById('sel2').value;
+	var divName = 'container2';
+
+	/*--------- 加载ECharts ---------*/
+	$.getJSON(fileNameBar, function(bardata) {
+		$.getJSON(fileNamePie, function(piedata) {
+			//合并到其他
+			var comObj = combine(bardata, piedata, 80, cityIdx);
+			var barDataObj = comObj.barDataObj;
+			var pieDataObj = comObj.pieDataObj;
+			var map = comObj.map;
+			loadPie(bardata, piedata, cityIdx, divName, barDataObj, pieDataObj, map);
+		});
+	});
+
+}
+
+function combine(bardata, piedata, percent, cityIdx) {
+	//从第几个开始合并
+	var accIdx;
+	for (var i = 0; i < bardata.bar.data.accumulative[cityIdx].length; i++) {
+		if (bardata.bar.data.accumulative[cityIdx][i] > percent) {
+			accIdx = i + 1;
+			break;
+		}
+	}
+
+	//计算bar其他类别的值
+	var otherVaue = 0;
+	for (var i = accIdx; i < bardata.bar.data.accumulative[cityIdx].length; i++) {
+		otherVaue += bardata.bar.data.value[i][cityIdx];
+	}
+
+	//生成bar的data对象
+	var barDataObj = [];
+	var obj;
+	for (var i = 0; i < accIdx; i++) {
+		obj = {
+			value : bardata.bar.data.value[i][cityIdx].toFixed(2),
+			name : bardata.bar.data.sector[i]
+		};
+		barDataObj.push(obj);
+	}
+	obj = {
+		value : otherVaue.toFixed(2),
+		name : "其他"
+	};
+	barDataObj.push(obj);
+
+	////计算pie其他类别的值
+	otherVaue = 0;
+	for (var i = 0; i < piedata.pie.data[cityIdx].name.length; i++) {
+		if (piedata.pie.data[cityIdx].mapping[i] >= accIdx && piedata.pie.data[cityIdx].value[i] != '-') {
+			otherVaue += piedata.pie.data[cityIdx].value[i];
+		}
+	}
+
+	//生成pie的data对象
+	var pieDataObj = [];
+	for (var i = 0; i < piedata.pie.data[cityIdx].name.length; i++) {
+		if (piedata.pie.data[cityIdx].mapping[i] < accIdx) {
+			var fixedValue;
+			if (piedata.pie.data[cityIdx].value[i] == '-') {
+				fixedValue = piedata.pie.data[cityIdx].value[i];
+			} else {
+				fixedValue = piedata.pie.data[cityIdx].value[i].toFixed(2);
+			}
+			obj = {
+				value : fixedValue,
+				name : piedata.pie.data[cityIdx].name[i]
+			}
+			pieDataObj.push(obj);
+		} else {
+			obj = {
+				value : otherVaue.toFixed(2),
+				name : "其他"
+			}
+			pieDataObj.push(obj);
+			break;
+		}
+	}
+
+	//计算map
+	var map = [];
+	console.log(piedata.pie.data[cityIdx].mapping.length);
+	for (var i = 0; i < piedata.pie.data[cityIdx].mapping.length; i++) {
+		if (piedata.pie.data[cityIdx].mapping[i] < accIdx) {
+			map.push(piedata.pie.data[cityIdx].mapping[i]);
+		} else {
+			map.push(accIdx);
+			break;
+		}
+	}
+
+	var comObj = {
+		barDataObj : barDataObj,
+		pieDataObj : pieDataObj,
+		map : map
+	}
+
+	return comObj;
+}
+
